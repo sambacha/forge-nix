0e2f8f550bf459d452906e07dc1dd24466f17412
sam bacha <sam@manifoldfinance.com>
2022-06-04 05:47:13 -0700

    feat(tests): release v0.0.3

---
+++ src/forge2.nix
@@ -0,0 +1,55 @@
+{ solidityPackage, solc, srcRoot ? null }:
+
+let
+  inherit (builtins) map listToAttrs attrNames attrValues length fromJSON readFile;
+  mapAttrs = if (builtins ? mapAttrs)
+    then builtins.mapAttrs
+    else f: attrs:
+      listToAttrs (map
+        (name: { inherit name; value = f name attrs."${name}"; })
+        (attrNames attrs));
+
+  defaults = {
+    inherit solc;
+  };
+
+  package = spec: let
+    spec' = defaults // (removeAttrs spec [ "repo" ]);
+    deps = map (spec:
+      package (spec // { inherit (spec') solc test-hevm hevm doCheck; })
+    ) (attrValues spec'.deps);
+  in solidityPackage (spec' // { inherit deps; });
+
+  packageSpecs = mapAttrs (_: package);
+
+  jsonSpecs = fromJSON (readFile ./.forge.json);
+
+  resolveDeps = root: _: v:
+    let
+      contract = jsonSpecs.contracts."${v}";
+      contract' = contract // {
+        src = if (srcRoot == null)
+          then "${fetchGit contract.repo}/src"
+          else "${srcRoot}/${root}/${contract.name}/src";
+      };
+      noDeps = length (attrNames contract.deps) == 0;
+    in
+      if noDeps
+      then contract'
+      else contract' // {
+        deps = mapAttrs
+          (resolveDeps "${root}/${contract.name}/lib")
+          contract.deps;
+      };
+
+  specs = (mapAttrs resolveDeps jsonSpecs.contracts) // {
+    this = jsonSpecs.this // {
+      src = ./.;
+      deps = mapAttrs (resolveDeps ".") jsonSpecs.this.deps;
+    };
+  };
+in {
+  inherit package packageSpecs specs;
+  this = package specs.this;
+  deps = packageSpecs specs.this.deps;
+}
