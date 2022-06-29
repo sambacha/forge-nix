0ee8a33b4f52ec4c94d2e9f4345c512d14208492
sam bacha <sam@manifoldfinance.com>
2022-06-29 03:38:34 -0700

    tools(checksum): ipfs hash

--- tools/make-checksum.sh
+++ tools/make-checksum.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 shell-ipfs < src/forge2nix > checksum.txt
 shell-ipfs -e hex -a sha2-256 -l 256  < src/forge2nix >> checksum.txt
+sleep 1
+
+shell-ipfs $(find . -name '*.sh') > IPFS_CHECKSUM.txt
+shell-ipfs -e hex -a sha2-256 -l 256 $(find . -name '*.sh') >> IPFS_CHECKSUM.txt
